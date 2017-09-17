367dca30b69004078abae9822e443532fe5cfc07
James Denman <james@fastmail.se>
2017-09-17 15:40:14 +1000

    testing the smart contract with Solidity tests

---
+++ test/TestAdoption.sol
@@ -0,0 +1,10 @@
+pragma Solidity ^0.4.11;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+  Adoption adoption = Adoption(deployedAddresses.adoption());
+
+}
