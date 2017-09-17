a43c20768364b04abc45d6a59fd2b4dc60d6e4ba
James Denman <james@fastmail.se>
2017-09-17 16:03:02 +1000

    tests passing

--- test/TestAdoption.sol
+++ test/TestAdoption.sol
@@ -1,11 +1,11 @@
-pragma Solidity ^0.4.11;
+pragma solidity ^0.4.11;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Adoption.sol";
 
 contract TestAdoption {
-  Adoption adoption = Adoption(deployedAddresses.adoption());
+  Adoption adoption = Adoption(DeployedAddresses.Adoption());
 
   function testUserCanAdoptPet() {
     uint returnedId = adoption.adopt(8);
@@ -14,4 +14,17 @@ contract TestAdoption {
     Assert.equal(returnedId, expected, "Adoption of pet ID 8 should be recorded.");
   }
 
+  function testGetAdopterAddressByPetId() {
+    address expected = this;
+    address adopter = adoption.adopters(8);
+
+    Assert.equal(adopter, expected, "Owner of pet ID 8 should be recorded.");
+  }
+
+  function testGetAdopterAddressByPetIdInArray() {
+    address expected = this;
+    address[16] memory adopters = adoption.getAdopters();
+
+    Assert.equal(adopters[8], expected, "Owner of pet ID 8 should be recorded.");
+  }
 }
